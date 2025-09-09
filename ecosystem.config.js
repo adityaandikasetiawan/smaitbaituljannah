88241d084a204aa468d101f7a3760a9905a3115b
adityaandikasetiawan <adityaandikasetiawan@gmail.com>
2025-09-09 17:18:38 +0700

    push

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   apps: [{
+<<<<<<< HEAD
     name: 'smait-baituljannah',
     script: './app.js',
     cwd: '/var/www/html/smaitbaituljannah',
@@ -25,3 +26,26 @@ module.exports = {
     max_memory_restart: '1G'
   }]
 };
+=======
+    name: 'smaitbaituljannah',
+    script: 'app.js',
+    cwd: '/var/www/html/smaitbaituljannah',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    },
+    error_file: '/var/log/pm2/smaitbaituljannah-error.log',
+    out_file: '/var/log/pm2/smaitbaituljannah-out.log',
+    log_file: '/var/log/pm2/smaitbaituljannah-combined.log',
+    time: true
+  }]
+};
+>>>>>>> 07ac1e6 (push)
